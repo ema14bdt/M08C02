850ff413044b25369fbc24d1089f8f6aef5063b5
ema14bdt <e.romero.1989@gmail.com>
2021-11-30 00:16:47 -0300

    Subo TP

---
+++ src/controllers/genresController.js
@@ -0,0 +1,16 @@
+const db = require('../database/models');
+
+const genresController = {
+    list: (req, res) => {
+        db.Genre.findAll().then((genres) => {
+            res.render('genresList.ejs', {genres});
+        });
+    },
+    detail: (req, res) => {
+        db.Genre.findByPk(req.params.id).then((genre) => {
+            res.render('genresDetail.ejs', {genre});
+        });
+    },
+};
+
+module.exports = genresController;
